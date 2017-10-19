a7cce7bb6ab6b592d5a2e283f3bff498eb89a573
tschaller <tony.schaller@medshare.net>
2017-10-19 18:40:06 +0200

    Timestamp calculation before destination variable setting

--- cda-ch-v2/createpdf-win.cmd
+++ cda-ch-v2/createpdf-win.cmd
@@ -7,12 +7,9 @@ REM Compatibility: Windows OS (adjust your Path variable in order to have the ac
 REM 
 REM History:
 REM 20171009: Initial commit by Tony Schaller, medshare GmbH
+REM 20171019: Timestamp calculation before destination variable setting (by Tony Schaller, medshare GmbH)
 REM 
 REM ********************************************************************************
-
-REM This is for customizing this script for the given specification
-SET Source=http://e-health-wiki.ch/index.php/Ehscda:CDA-CH_2017_(specification)
-SET Destination=CDA-CH_de_V%DT%.pdf
 
 REM Please do not change the lines below
 SET DD=%date:~0,2%
@@ -20,4 +17,10 @@ SET MM=%date:~3,2%
 SET YY=%date:~8,2%
 SET YYYY=%date:~6,4%
 SET DT=%YYYY%%MM%%DD%
+
+REM This is for customizing this script for the given specification
+SET Source=http://e-health-wiki.ch/index.php/Ehscda:CDA-CH_2017_(specification)
+SET Destination=CDA-CH_de_V%DT%.pdf
+
+REM Please do not change the lines below
 prince.exe "%Source%" -v --script makeruler.js --javascript -o %Destination% -s "http://e-health-wiki.ch/index.php?title=MediaWiki:Common.css&usemsgcache=yes&ctype=text%2Fcss&smaxage=18000&action=raw&maxage=18000"   -s "http://e-health-wiki.ch/index.php?title=-&action=raw&maxage=18000&gen=css"   -s "http://e-health-wiki.ch/index.php?title=MediaWiki:Geshi.css&usemsgcache=yes&action=raw&ctype=text/css&smaxage=18000" -s kprint3ohneruler.css
