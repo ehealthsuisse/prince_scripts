31b896b5d807e9b724f83a3db6b0caa61bf875b6
Johannes Gnägi <johannes.gnaegi@e-health-suisse.ch>
2018-04-18 17:11:35 +0200

    Updated CSS Styles - now global CSS style for CDA-CH and future specification documents. Windows scripts need to be adapted according to the osx-shell script.

--- cda-ch-v2/createpdf-osx.sh
+++ cda-ch-v2/createpdf-osx.sh
@@ -5,6 +5,8 @@
 # Compatibility: Unix (OS X) 
 # History:
 # 20171024: Setting up shell script based on script from T. Schaller, medshare GmbH
+# 20180418: Germand and French output, unique CSS file for styling and localized CSS
+# for German/French output from Johannes Gnaegi, eHealth Suisse
 # 
 # ********************************************************************************
 
@@ -12,9 +14,21 @@
 d=$(date +%Y%m%d)
 echo $d
 
+# ******************
+# GERMAN
 # This is for customizing this script for the given specification
 Source="http://e-health-wiki.ch/index.php/Ehscda:CDA-CH_2017_(specification)"
 Destination="CDA-CH_de_V$d.pdf"
+germanStyle=../lang_de.css
 
 # Please do not change the lines below
-prince $Source -v --script makeruler.js --javascript -o $Destination -s "http://e-health-wiki.ch/index.php?title=MediaWiki:Common.css&usemsgcache=yes&ctype=text%2Fcss&smaxage=18000&action=raw&maxage=18000"   -s "http://e-health-wiki.ch/index.php?title=-&action=raw&maxage=18000&gen=css"   -s "http://e-health-wiki.ch/index.php?title=MediaWiki:Geshi.css&usemsgcache=yes&action=raw&ctype=text/css&smaxage=18000" -s kprint3ohneruler.css
+prince $Source -v --script makeruler.js --javascript -o $Destination -s "http://e-health-wiki.ch/index.php?title=MediaWiki:Common.css&usemsgcache=yes&ctype=text%2Fcss&smaxage=18000&action=raw&maxage=18000"   -s "http://e-health-wiki.ch/index.php?title=-&action=raw&maxage=18000&gen=css"   -s "http://e-health-wiki.ch/index.php?title=MediaWiki:Geshi.css&usemsgcache=yes&action=raw&ctype=text/css&smaxage=18000" -s $germanStyle -s ../kprint3ohneruler.css
+
+# ******************
+# FRENCH
+# This is for customizing this script for the given specification
+Source="http://e-health-wiki.ch/index.php/Ehscda:CDA-CH_2017_(specification)/fr"
+Destination="CDA-CH_fr_V$d.pdf"
+frenchStyle=../lang_fr.css
+
+prince $Source -v --script makeruler.js --javascript -o $Destination -s "http://e-health-wiki.ch/index.php?title=MediaWiki:Common.css&usemsgcache=yes&ctype=text%2Fcss&smaxage=18000&action=raw&maxage=18000"   -s "http://e-health-wiki.ch/index.php?title=-&action=raw&maxage=18000&gen=css"   -s "http://e-health-wiki.ch/index.php?title=MediaWiki:Geshi.css&usemsgcache=yes&action=raw&ctype=text/css&smaxage=18000" -s $frenchStyle -s ../kprint3ohneruler.css
