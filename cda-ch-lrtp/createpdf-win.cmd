3a0106e7c00d7d85071b73cafd1886c56e733a4e
tschaller <tony.schaller@medshare.net>
2018-05-07 18:54:41 +0200

    Supporting french versions in windows cmds

--- cda-ch-lrtp/createpdf-win.cmd
+++ cda-ch-lrtp/createpdf-win.cmd
@@ -8,6 +8,7 @@ REM
 REM History:
 REM 20171009: Initial commit by Tony Schaller, medshare GmbH
 REM 20171019: Timestamp calculation before destination variable setting (by Tony Schaller, medshare GmbH)
+REM 20180507: Update for french version and updated css definitions (by Tony Schaller, medshare GmbH)
 REM 
 REM ********************************************************************************
 
@@ -17,10 +18,19 @@ SET MM=%date:~3,2%
 SET YY=%date:~8,2%
 SET YYYY=%date:~6,4%
 SET DT=%YYYY%%MM%%DD%
+SET masterStyle=../eheathwiki_pdfstyle.css
+SET rulerScript=../makeruler.js
+SET germanStyle=../lang_de.css
+SET frenchStyle=../lang_fr.css
 
 REM This is for customizing this script for the given specification
-SET Source=http://e-health-wiki.ch/index.php/Ehscda:CDA-CH-LRTP_(specification)
-SET Destination=CDA-CH-LRTP_de_V%DT%.pdf 
+SET SourceDe=http://e-health-wiki.ch/index.php/Ehscda:CDA-CH-LRTP_(specification)
+SET SourceFr=http://e-health-wiki.ch/index.php/Ehscda:CDA-CH-LRTP_(specification)/fr
+SET DestinationDe=CDA-CH-LRTP_de_V%DT%.pdf 
+SET DestinationFr=CDA-CH-LRTP_fr_V%DT%.pdf 
+ECHO %DestinationDe%
+ECHO %DestinationFr%
 
 REM Please do not change the lines below
-prince.exe "%Source%" -v --script makeruler.js --javascript -o %Destination% -s "http://e-health-wiki.ch/index.php?title=MediaWiki:Common.css&usemsgcache=yes&ctype=text%2Fcss&smaxage=18000&action=raw&maxage=18000"   -s "http://e-health-wiki.ch/index.php?title=-&action=raw&maxage=18000&gen=css"   -s "http://e-health-wiki.ch/index.php?title=MediaWiki:Geshi.css&usemsgcache=yes&action=raw&ctype=text/css&smaxage=18000" -s kprint3ohneruler.css
+prince.exe "%SourceDe%" -v --script %rulerScript% --javascript -o %DestinationDe% -s "http://e-health-wiki.ch/index.php?title=MediaWiki:Common.css&usemsgcache=yes&ctype=text%2Fcss&smaxage=18000&action=raw&maxage=18000"   -s "http://e-health-wiki.ch/index.php?title=-&action=raw&maxage=18000&gen=css"   -s "http://e-health-wiki.ch/index.php?title=MediaWiki:Geshi.css&usemsgcache=yes&action=raw&ctype=text/css&smaxage=18000" -s %germanStyle% -s %masterStyle%
+prince.exe "%SourceFr%" -v --script %rulerScript% --javascript -o %DestinationFr% -s "http://e-health-wiki.ch/index.php?title=MediaWiki:Common.css&usemsgcache=yes&ctype=text%2Fcss&smaxage=18000&action=raw&maxage=18000"   -s "http://e-health-wiki.ch/index.php?title=-&action=raw&maxage=18000&gen=css"   -s "http://e-health-wiki.ch/index.php?title=MediaWiki:Geshi.css&usemsgcache=yes&action=raw&ctype=text/css&smaxage=18000" -s %frenchStyle% -s %masterStyle%
--- cda-ch-v2/createpdf-win.cmd
+++ cda-ch-v2/createpdf-win.cmd
@@ -8,7 +8,7 @@ REM
 REM History:
 REM 20171009: Initial commit by Tony Schaller, medshare GmbH
 REM 20171019: Timestamp calculation before destination variable setting (by Tony Schaller, medshare GmbH)
-REM 20180419: Add support for exporting both: Germand and French version (by Johannes Gnaegi, eHealth Suisse)
+REM 20180507: Update for french version and updated css definitions (by Tony Schaller, medshare GmbH)
 REM 
 REM ********************************************************************************
 
@@ -18,22 +18,19 @@ SET MM=%date:~3,2%
 SET YY=%date:~8,2%
 SET YYYY=%date:~6,4%
 SET DT=%YYYY%%MM%%DD%
-SET MasterStyle=..\eheathwiki_pdfstyle.css
-SET RulerScript=..\makeruler.js
+SET masterStyle=../eheathwiki_pdfstyle.css
+SET rulerScript=../makeruler.js
+SET germanStyle=../lang_de.css
+SET frenchStyle=../lang_fr.css
 
 REM This is for customizing this script for the given specification
-SET Source=http://e-health-wiki.ch/index.php/Ehscda:CDA-CH_2017_(specification)
-
-REM GERMAN Version
-SET DestinationDE=CDA-CH_de_V%DT%.pdf
-SET GermanStyle=..\lang_de.css
-
-REM Please do not change the lines below
-prince.exe "%Source%" -v --script "%RulerScript%" --javascript -o %DestinationDE% -s "http://e-health-wiki.ch/index.php?title=MediaWiki:Common.css&usemsgcache=yes&ctype=text%2Fcss&smaxage=18000&action=raw&maxage=18000"   -s "http://e-health-wiki.ch/index.php?title=-&action=raw&maxage=18000&gen=css"   -s "http://e-health-wiki.ch/index.php?title=MediaWiki:Geshi.css&usemsgcache=yes&action=raw&ctype=text/css&smaxage=18000" -s "%FrenchStyle%" -s "%MasterStyle%"
-
-REM FRENCH Version
-SET DestinationFR=CDA-CH_fr_V%DT%.pdf
-SET FrenchStyle=..\lang_fr.css
+SET SourceDe=http://e-health-wiki.ch/index.php/Ehscda:CDA-CH_2017_(specification)
+SET SourceFr=http://e-health-wiki.ch/index.php/Ehscda:CDA-CH_2017_(specification)/fr
+SET DestinationDe=CDA-CH_de_V%DT%.pdf 
+SET DestinationFr=CDA-CH_fr_V%DT%.pdf 
+ECHO %DestinationDe%
+ECHO %DestinationFr%
 
 REM Please do not change the lines below
-prince.exe "%Source%" -v --script "%RulerScript%" --javascript -o %DestinationFR% -s "http://e-health-wiki.ch/index.php?title=MediaWiki:Common.css&usemsgcache=yes&ctype=text%2Fcss&smaxage=18000&action=raw&maxage=18000"   -s "http://e-health-wiki.ch/index.php?title=-&action=raw&maxage=18000&gen=css"   -s "http://e-health-wiki.ch/index.php?title=MediaWiki:Geshi.css&usemsgcache=yes&action=raw&ctype=text/css&smaxage=18000" -s "%FrenchStyle%" -s "%MasterStyle%"
+prince.exe "%SourceDe%" -v --script %rulerScript% --javascript -o %DestinationDe% -s "http://e-health-wiki.ch/index.php?title=MediaWiki:Common.css&usemsgcache=yes&ctype=text%2Fcss&smaxage=18000&action=raw&maxage=18000"   -s "http://e-health-wiki.ch/index.php?title=-&action=raw&maxage=18000&gen=css"   -s "http://e-health-wiki.ch/index.php?title=MediaWiki:Geshi.css&usemsgcache=yes&action=raw&ctype=text/css&smaxage=18000" -s %germanStyle% -s %masterStyle%
+prince.exe "%SourceFr%" -v --script %rulerScript% --javascript -o %DestinationFr% -s "http://e-health-wiki.ch/index.php?title=MediaWiki:Common.css&usemsgcache=yes&ctype=text%2Fcss&smaxage=18000&action=raw&maxage=18000"   -s "http://e-health-wiki.ch/index.php?title=-&action=raw&maxage=18000&gen=css"   -s "http://e-health-wiki.ch/index.php?title=MediaWiki:Geshi.css&usemsgcache=yes&action=raw&ctype=text/css&smaxage=18000" -s %frenchStyle% -s %masterStyle%
