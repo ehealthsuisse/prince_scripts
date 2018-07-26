99c661138f2df2d41da9cf3c7e685acce6a79907
Johannes Gnägi <johannes.gnaegi@e-health-suisse.ch>
2018-07-26 15:59:20 +0200

    changes after mediawiki update from version 1.26 > 1.31

--- testsite/createpdf-osx.sh
+++ testsite/createpdf-osx.sh
@@ -13,6 +13,7 @@
 # Please do not change the lines below
 d=$(date +%y%m%d)
 echo $d
+#masterStyle=../wiki2.css
 masterStyle=../eheathwiki_pdfstyle.css
 rulerScript=../makeruler.js
 
@@ -24,13 +25,13 @@ Destination=$d"_Testsite_de.pdf"
 germanStyle=../lang_de.css
 
 # Please do not change the lines below
-prince $Source -v --script $rulerScript --javascript -o $Destination -s "http://e-health-wiki.ch/index.php?title=MediaWiki:Common.css&usemsgcache=yes&ctype=text%2Fcss&smaxage=18000&action=raw&maxage=18000"   -s "http://e-health-wiki.ch/index.php?title=-&action=raw&maxage=18000&gen=css"   -s "http://e-health-wiki.ch/index.php?title=MediaWiki:Geshi.css&usemsgcache=yes&action=raw&ctype=text/css&smaxage=18000" -s $germanStyle -s $masterStyle
+prince $Source -v --script $rulerScript --javascript -o $Destination -s "http://e-health-wiki.ch/index.php?title=MediaWiki:Common.css&usemsgcache=yes&ctype=text%2Fcss&smaxage=18000&action=raw&maxage=18000" -s "http://e-health-wiki.ch/index.php?title=MediaWiki:Geshi.css&usemsgcache=yes&action=raw&ctype=text/css&smaxage=18000" -s $germanStyle -s $masterStyle
 
 # ******************
 # FRENCH
 # This is for customizing this script for the given specification
-Source="http://e-health-wiki.ch/index.php/Testsite/fr"
-Destination=$d"_Testsite_fr.pdf"
-frenchStyle=../lang_fr.css
+#Source="http://wiki.hl7.de/index.php?title=IG:Arztbrief_Plus"
+#Destination=$d"_Testsite_fr.pdf"
+#frenchStyle=../lang_fr.css
 
-prince $Source -v --script $rulerScript --javascript -o $Destination -s "http://e-health-wiki.ch/index.php?title=MediaWiki:Common.css&usemsgcache=yes&ctype=text%2Fcss&smaxage=18000&action=raw&maxage=18000"   -s "http://e-health-wiki.ch/index.php?title=-&action=raw&maxage=18000&gen=css"   -s "http://e-health-wiki.ch/index.php?title=MediaWiki:Geshi.css&usemsgcache=yes&action=raw&ctype=text/css&smaxage=18000" -s $frenchStyle -s $masterStyle
+#prince $Source --script $rulerScript --javascript -o $Destination -s "http://wiki.hl7.de/index.php?title=MediaWiki:Common.css&usemsgcache=yes&ctype=text%2Fcss&smaxage=18000&action=raw&maxage=18000"   -s "http://wiki.hl7.de/index.php?title=-&action=raw&maxage=18000&gen=css"   -s "http://wiki.hl7.de/index.php?title=MediaWiki:Geshi.css&usemsgcache=yes&action=raw&ctype=text/css&smaxage=18000" -s $frenchStyle -s $masterStyle
